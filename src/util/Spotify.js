ee5493d9fe3a0e50bb5cb49ef391417e81232a22
System Administrator <root@Christians-MacBook-Pro.local>
2018-11-23 19:54:29 -0500

    second push

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -30,7 +30,7 @@ const Spotify = {
   search(term) {
     this.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
-      headers: {Authorization: Bearer ${accessToken}}
+      headers: {Authorization: `Bearer ${accessToken}`}
     }).then(response => {
       return response.json();
     }).then(response => {
