eba2d38c9fc80d178fd62c0b5584e88b1841bc89
System Administrator <root@Christians-MacBook-Pro.local>
2018-11-24 19:31:54 -0500

    Updates

---
+++ precache-manifest.34c1e96fecabd9a757487ed431f618de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jamming/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "0954bfa69d8c958ca104",
+    "url": "/jamming/static/js/runtime~main.0954bfa6.js"
+  },
+  {
+    "revision": "a38158a770c73eadd1d5",
+    "url": "/jamming/static/js/main.a38158a7.chunk.js"
+  },
+  {
+    "revision": "6690267cc1d0633ef44a",
+    "url": "/jamming/static/js/1.6690267c.chunk.js"
+  },
+  {
+    "revision": "a38158a770c73eadd1d5",
+    "url": "/jamming/static/css/main.bab71197.chunk.css"
+  },
+  {
+    "revision": "400e706c2ea974cf78734434a57dee00",
+    "url": "/jamming/index.html"
+  }
+];
