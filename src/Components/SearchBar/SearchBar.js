391f2cf060e8b3a5dd20601266c02e8176384557
System Administrator <root@Christians-MacBook-Pro.local>
2018-12-05 19:58:44 -0500

    Added enter key search event handler

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -7,6 +7,7 @@ class SearchBar extends React.Component {
 
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   search() {
@@ -17,10 +18,18 @@ class SearchBar extends React.Component {
     this.setState({term: event.target.value});
   }
 
+  handleSearch(event) {
+    if(event.keyCode === 13) {
+      this.search()
+    }
+
+  }
+
   render() {
     return (
       <div className="SearchBar">
         <input onChange={this.handleTermChange}
+        onKeyDown={this.handleSearch}
         placeholder="Enter A Song, Album, or Artist" />
         <a onClick={this.search} >SEARCH</a>
       </div>
